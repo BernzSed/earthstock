19291ac6cbbcb827201862385859f9aab85c183f
Bernie Stern <bernzs@gmail.com>
2014-05-10 16:14:15 -0400

    changes to quote

--- earthstock/models.py
+++ earthstock/models.py
@@ -12,7 +12,7 @@ class Stock(models.Model):
     longitude = models.FloatField()
 
 class Quote(models.Model):
-    stock = models.ForeignKey('Stock')
+    stock = models.ForeignKey('Stock', null=False, unique=True)
     price = models.FloatField()
     change = models.FloatField()
     last_modified = models.DateTimeField(null=False, auto_now=True, auto_now_add=True)
