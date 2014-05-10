3f43d0862d4dea128377e9e13540b1afc5b3904b
Bernie Stern <bernzs@gmail.com>
2014-05-10 12:29:34 -0400

    models

---
+++ earthstock/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+
+class Stock(models.Model):
+    symbol = models.CharField(max_length = 50, null=False)
+    company = models.CharField(max_length = 200, null=False)
+    latitude = models.DecimalField()
+    longitude = models.DecimalField()
