895b0a5893c2bd59a8d7554a5eebf4bfe5e1e5d9
Bernie Stern <bernzs@gmail.com>
2014-05-10 15:50:25 -0400

    model unicode representation

--- earthstock/models.py
+++ earthstock/models.py
@@ -3,6 +3,9 @@ from django.db import models
 # Create your models here.
 
 class Stock(models.Model):
+    def __unicode__(self):
+        return u'%s \"%s\" (%.2f, %.2f)' % (self.symbol, self.company, self.latitude, self.longitude)
+    
     symbol = models.CharField(max_length = 50, null=False)
     company = models.CharField(max_length = 200, null=False)
     latitude = models.FloatField()
